87a79364756acd19e586b4f23b7a9ad9f3978d53
Kirill Maslov <ksmaslov@mail.ru>
2023-10-15 17:03:58 +0300

    gcd + log duration

--- main.cpp
+++ main.cpp
@@ -1,10 +1,46 @@
-
+#include <chrono>
+#include <cassert>
+#include <cstdint>
 #include <iostream>
 
-using namespace std;
+class LogDuration
+{
+public:
+    LogDuration() {
+    }
+    ~LogDuration() {
+        const auto end_time = std::chrono::steady_clock::now();
+        const auto dur = end_time - start_time_;
+
+        std::cerr << std::chrono::duration_cast<std::chrono::milliseconds>(dur).count() << " ms" << std::endl;
+    }
+private:
+    const std::chrono::steady_clock::time_point start_time_ = std::chrono::steady_clock::now();
+};
+
+template <class Int>
+Int gcd(Int a, Int b)
+{
+    while (a > 0 && b > 0)
+    {
+        if (a > b)
+            a %= b;
+        else
+            b %= a;
+    }
+
+    return a == 0 ? b : a;
+}
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int64_t n;
+    int64_t m;
+    std::cin >> n >> m;
+
+    LogDuration log_duration;
+
+    std::cout << gcd(n, m) << std::endl;
+
     return 0;
 }
